d91d3f1e47df8dd3d9fb4840645de27a96d58fd6
Artem Shurshilov <shurshilov.a@yandex.ru>
2024-08-04 11:27:04 +0300

    [REF] док стринг

--- dotorm/exceptions.py
+++ dotorm/exceptions.py
@@ -1,57 +1,9 @@
-""" Exceptions related to api"""
+""" Exceptions related to orm and builder"""
 
 
 class OrmConfigurationFieldException(Exception):
-    """Exception that raised when called api"""
+    """Exception that raised when wrong config model or fields"""
 
 
 class OrmUpdateEmptyParamsException(Exception):
-    """Exception that raised when called api"""
-
-
-class OrmExecutorException(Exception):
-    """Exception that raised when called api"""
-
-
-class OrmExecutorFirstTaskException(Exception):
-    """Exception that raised when called api"""
-
-
-class MysqlGetConnectionExecuteException(Exception):
-    """Exception that raised when mysql query error"""
-
-
-class MysqlQueryExecuteException(Exception):
-    """Exception that raised when mysql query error"""
-
-    def __init__(self, cmd: str, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.cmd = cmd
-
-
-class MysqlConnectionExecuteException(Exception):
-    """Exception that raised when mysql connection query error"""
-
-    def __init__(self, cmd: str, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.cmd = cmd
-
-
-class PostgresGetConnectionExecuteException(Exception):
-    """Exception that raised when Postgres query error"""
-
-
-class PostgresQueryExecuteException(Exception):
-    """Exception that raised when Postgres query error"""
-
-    def __init__(self, cmd: str, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.cmd = cmd
-
-
-class PostgresConnectionExecuteException(Exception):
-    """Exception that raised when Postgres connection query error"""
-
-    def __init__(self, cmd: str, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.cmd = cmd
+    """Exception that raised when orm not have required params"""
